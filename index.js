35e062bed2f3a0621e78a73e9777916bbc863e81
TeerapatJuheng <141821227+TeerapatJuheng@users.noreply.github.com>
2024-08-26 15:08:25 +0700

    commit

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const qrcode = require('qrcode'); // Assuming you're using qrcode package
+
 const app = express();
-const QRCode = require('qrcode')
-const generatePayload = require('promptpay-qr')
-const bodyParser = require('body-parser')
-const _ = require('lodash')
-const cors = require('cors')
+const port = 3000;
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded( { extended: true }))
-
-const server = app.listen(3000, () => {
-    console.log('server is running........')
-})
+app.use(bodyParser.json());
 
 app.post('/generateQR', (req, res) => {
-    const amount = parseFloat(_.get(req, ["body", "amount"]));
-    const mobileNumber = '0123456789';
-    const payload = generatePayload(mobileNumber, { amount });
-    const option = {
-        color: {
-            dark: '#000',
-            light: '#fff'
+    const { amount } = req.body;
+    
+    // Generate QR code URL or data
+    qrcode.toDataURL(`Amount: ${amount}`, (err, url) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to generate QR code' });
         }
-    }
-    QRCode.toDataURL(payload, option, (err, url) => {
-        if(err) {
-            console.log('generate fail')
-            return res.status(400).json({
-                RespCode: 400,
-                RespMessage: 'bad : ' + err
-            })  
-        } 
-        else {
-            return res.status(200).json({
-                RespCode: 200,
-                RespMessage: 'good',
-                Result: url
-            })  
-        }
+        res.json({ Result: url });
+    });
+});
 
-    })
-})
-
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
