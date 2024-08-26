6ed2113eebc48a8617a21cab01115fd13a6015e3
TeerapatJuheng <141821227+TeerapatJuheng@users.noreply.github.com>
2024-08-26 16:47:25 +0700

    commit

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const promptpay = require('promptpay-qr');
-
+const express = require('express')
 const app = express();
-const port = 3000; // Change to a different port
+const QRCode = require('qrcode')
+const generatePayload = require('promptpay-qr')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
+const cors = require('cors')
 
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded( { extended: true }))
+
+const server = app.listen(3000, () => {
+    console.log('server is running........')
+})
 
 app.post('/generateQR', (req, res) => {
-    const { amount } = req.body;
-    
-    if (!amount || isNaN(amount) || amount <= 0) {
-        return res.status(400).json({ error: 'Invalid amount' });
+    const amount = parseFloat(_.get(req, ["body", "amount"]));
+    const mobileNumber = '0615927954';
+    const payload = generatePayload(mobileNumber, { amount });
+    const option = {
+        color: {
+            dark: '#000',
+            light: '#fff'
+        }
     }
+    QRCode.toDataURL(payload, option, (err, url) => {
+        if(err) {
+            console.log('generate fail')
+            return res.status(400).json({
+                RespCode: 400,
+                RespMessage: 'bad : ' + err
+            })  
+        } 
+        else {
+            return res.status(200).json({
+                RespCode: 200,
+                RespMessage: 'good',
+                Result: url
+            })  
+        }
 
-    const accountNumber = '0615927954';
-    const qrCodeImage = promptpay(accountNumber, amount);
+    })
+})
 
-    res.writeHead(200, {
-        'Content-Type': 'image/png',
-        'Content-Disposition': 'inline; filename="promptpay-qr.png"'
-    });
-    res.end(qrCodeImage);
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+module.exports = app;
